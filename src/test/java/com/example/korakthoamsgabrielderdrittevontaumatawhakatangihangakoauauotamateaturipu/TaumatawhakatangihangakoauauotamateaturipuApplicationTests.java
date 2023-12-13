7cbfe136d7465252b81a17913e1f2a94ed26f26d
Time_ON <haas.timeon@gmail.com>
2023-12-13 09:37:40 +0100

    GET/POST/PUT/DELETE HttpMethoden added

---
+++ src/test/java/com/example/korakthoamsgabrielderdrittevontaumatawhakatangihangakoauauotamateaturipu/TaumatawhakatangihangakoauauotamateaturipuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.korakthoamsgabrielderdrittevontaumatawhakatangihangakoauauotamateaturipu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaumatawhakatangihangakoauauotamateaturipuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
